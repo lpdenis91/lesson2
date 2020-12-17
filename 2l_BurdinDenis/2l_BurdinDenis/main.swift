72ab5ab9fd56b3fb2d9caf85d0a38f773387d155
Denis Burdin <lpdenis91@gmail.com>
2020-12-17 14:44:48 +0500

    Homework
    Задания с 1 по 5

--- 2l_BurdinDenis/2l_BurdinDenis/main.swift
+++ 2l_BurdinDenis/2l_BurdinDenis/main.swift
@@ -9,3 +9,57 @@ import Foundation
 
 print("Hello, World!")
 
+
+//Написать функцию которая определяет четное число или нет
+
+func evenOrOdd(digit: Int) {
+    if digit % 2 == 0 {
+        digitsHund.remove(at: digitsHund.firstIndex(of: digit)!)
+    }
+}
+
+//Написать функцию, которая определяет, делится ли число без остатка на 3
+
+func divisionBy3(dig: Int) {
+    if dig%3 == 0 {
+        if digitsHund.contains(dig) {
+            digitsHund.remove(at: digitsHund.firstIndex(of: dig)!)
+        }
+    }
+}
+
+//Возрастающий массив из 100 чисел - Задание 3
+var digitsHund: [Int] = []
+for i in 1...100 {
+    digitsHund.append(i)
+}
+
+print("\(digitsHund)\n")
+
+for i in digitsHund {
+    evenOrOdd(digit: i)
+    divisionBy3(dig: i)
+}
+
+print("Массив после изменений: \(digitsHund)\n")
+
+//
+func fibonacciCalc(dig: Int) {
+    let a: Int = dig - 1
+    let b: Int = dig - 2
+    if a >= 0 && b >= 0 {
+        fibonacciArr.append(fibonacciArr[a] + fibonacciArr[b])
+    } else if a >= 0 && b < 0 {
+        fibonacciArr.append(fibonacciArr[a])
+    } else {
+        fibonacciArr.append(1)
+    }
+
+}
+
+var fibonacciArr: [Double] = []
+for i in 0...100 {
+    fibonacciCalc(dig: i)
+}
+
+print("Последовательность Фибоначчи: \(fibonacciArr)")
